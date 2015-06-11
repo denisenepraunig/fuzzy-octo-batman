cddf12abcf2d8cba00a9681d4d4228a408995b19
d062712 <denise.nepraunig@sap.com>
2015-06-11 20:15:35 +0000

    rename simple to pages to xml and js view

---
+++ app-simple-two-page-view-xml/webapp/controller/Master.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sapui5.demo.mvcapp.controller.Master", {
+	    
+	    onListPress : function(oEvent) {
+                
+            var sPageId = "detailPage";
+            oApp.to(sPageId);
+            
+            var oPage = oApp.getPage(sPageId);
+            var oContext = oEvent.getSource().getBindingContext();
+		    oPage.setBindingContext(oContext);
+        }
+	});
+});
+
+// would be possible, but we still have
+// global oApp, and who knows for sure
+// that we getParent will return sap.m.App
+// var oApp = this.getView().getParent();

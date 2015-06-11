a11a0d9997c25882b04c2e734c17cf69d5cea900
d062712 <denise.nepraunig@sap.com>
2015-06-11 22:03:51 +0000

    corrections fixes whatever it is very late

---
+++ app-simple-three-page-view-xml-js/webapp/controller/Master.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sapui5.demo.mvcapp.controller.Master", {
+	    
+	    onListPress : function(oEvent) {
+                
+            var sPageId = "detailPage";
+            oApp.to(sPageId);
+            
+            var oPage = oApp.getPage(sPageId);
+            var oContext = oEvent.getSource().getBindingContext();
+		    oPage.setBindingContext(oContext);
+        }
+	});
+});

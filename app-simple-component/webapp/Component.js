8c3765aba791bf35fc021576ad4ca33f7b29db8d
denisenepraunig <denise.nepraunig@gmail.com>
2015-06-14 00:48:55 +0000

    component coding - mixed - needs to be separated...

---
+++ app-simple-component/webapp/Component.js
@@ -0,0 +1,65 @@
+sap.ui.define([
+		"sap/ui/core/UIComponent",
+		"sap/ui/model/json/JSONModel"
+	], function (UIComponent, JSONModel) {
+	"use strict";
+
+	return UIComponent.extend("sapui5.demo.mvcapp.Component", {
+
+    	metadata : {
+    		rootView : "sapui5.demo.mvcapp.view.App",
+    		dependencies : {
+    			libs : [
+    				"sap.m"
+    			]
+    		}
+    	},
+		
+    	createContent : function() {
+            var oData = {
+                "CountSuppliers" : "2",
+                "Suppliers":[  
+                    {  
+                        "ID":0,
+                        "Name":"Exotic Liquids",
+                        "Address":{  
+                            "Street":"NE 228th",
+                            "City":"Sammamish",
+                            "State":"WA",
+                            "ZipCode":"98074",
+                            "Country":"USA"
+                        }
+                    },
+                    {  
+                        "ID":1,
+                        "Name":"Tokyo Traders",
+                        "Address":{  
+                            "Street":"NE 40th",
+                            "City":"Redmond",
+                            "State":"WA",
+                            "ZipCode":"98052",
+                            "Country":"USA"
+                        }
+                    }
+                ]
+            };
+            
+            var oModel = new JSONModel();
+            oModel.setData(oData);
+            
+            // important to set the model on the component
+            // and not on the sapui5 core!!!!
+            this.setModel(oModel);
+
+			// call the base component's createContent function
+			//var oRootView = UIComponent.prototype.createContent.apply(this, arguments);
+			var oRootView = sap.ui.view("appview", { 
+                type: sap.ui.core.mvc.ViewType.XML,
+                viewName: "sapui5.demo.mvcapp.view.App"
+            });
+			
+			oApp = oRootView.byId("app");
+			return oRootView;
+		}
+	});
+});
---
+++ app-simple-component/webapp/view/Master.view.js
@@ -0,0 +1,57 @@
+sap.ui.jsview("sapui5.demo.mvcapp.view.Master", {
+   
+   getControllerName: function() {
+       
+      return "sapui5.demo.mvcapp.controller.Master";
+   },
+
+   createContent: function(oController) {
+       
+        var aColumns = [ 
+            new sap.m.Column({
+                header : new sap.m.Text({
+				    text : "ID"
+			    })
+            }),
+            new sap.m.Column({
+                header : new sap.m.Text({
+				    text : "Name"
+			    })
+            })
+        ];
+        
+        var oTemplate = new sap.m.ColumnListItem({
+            type: "Navigation",
+            press: [oController.onListPress, oController],
+    		cells : [
+    			new sap.m.ObjectIdentifier({
+    				text : "{ID}"
+    			}),
+    			new sap.m.ObjectIdentifier({
+    				text : "{Name}"
+    			})
+    		]
+    	});
+    	
+    	var oTableHeader = new sap.m.Toolbar({
+		    content : [
+    			new sap.m.Title({
+    				text : "Number of Suppliers: {/CountSuppliers}"
+    			})
+    		]
+	    });
+        
+        var oTable = new sap.m.Table({
+            columns : aColumns,
+            headerToolbar : oTableHeader
+        });
+        
+        oTable.bindItems("/Suppliers", oTemplate);
+        
+        var oPageMaster = new sap.m.Page({
+    		title : "Supplier Overview",
+    		content : [oTable]
+    	});
+    	return oPageMaster;
+   }
+});

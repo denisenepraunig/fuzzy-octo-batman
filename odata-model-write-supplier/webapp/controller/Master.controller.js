e3fbdc3902c4b39e8de11445ee879076cfafbc21
denisenepraunig <denise.nepraunig@gmail.com>
2016-01-11 12:35:06 +0000

    chapter 7 - odata write

--- odata-model-write-supplier/webapp/controller/Master.controller.js
+++ odata-model-write-supplier/webapp/controller/Master.controller.js
@@ -1,123 +1,90 @@
 sap.ui.define([
-		"sapui5/demo/odata/readingdata/bestpractice/controller/BaseController"
-	], function (BaseController) {
+	"sapui5/demo/odata/readingdata/bestpractice/controller/BaseController",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function(BaseController, MessageToast, MessageBox) {
 	"use strict";
 
 	return BaseController.extend("sapui5.demo.odata.readingdata.bestpractice.controller.Master", {
 
 		onInit: function() {
-			
+
 			this.getRouter().getRoute("master").attachPatternMatched(this.onAdd, this);
-			
-			// this.getModel() of BaseController does really NOT work...
+			// this.getModel() of BaseController does not work here!
 			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
 		},
-		
+
 		_onMetadataLoaded: function() {
-			
+
 			this._bMetadtaLoaded = true;
-			console.log("metadata loaded");
-			
 			if (this._bRoutingReady) {
 				this._createEntry();
 			}
 		},
-		
+
 		onAdd: function() {
-			
+
 			this._bRoutingReady = true;
-			console.log("routing ready");
-			
+
 			if (this._bMetadtaLoaded) {
 				this._createEntry();
 			}
 		},
-		
+
 		_createEntry: function() {
-			
+
 			var oModel = this.getModel();
 			var that = this;
-			
+
 			var oView = that.getView();
-				var oContext = oModel.createEntry("Suppliers", {
-					success : that._onCreateEntrySuccess.bind(that),
-					error : that._onCreateEntryError.bind(that) 
-				});
-				oView.setBindingContext(oContext);
+			var oContext = oModel.createEntry("Suppliers", {
+				success: that._onCreateEntrySuccess.bind(that),
+				error: that._onCreateEntryError.bind(that)
+			});
+			oView.setBindingContext(oContext);
 		},
-		
-		
+
+		_onCreateEntrySuccess: function(oObject) {
+			console.log("onCreateEntrySuccess");
+		},
+		_onCreateEntryError: function(oObject) {
+			console.log("onCreateEntryError");
+		},
+
 		_getPathInfo: function() {
-			
+
 			if (!this._sPath) {
 				var oPage = this.getView().getBindingContext();
-				
+
 				this._sPath = oPage.getBindingContext().getPath();
 			}
 			return this._sPath;
 		},
-		
-		_onCreateEntrySuccess : function(oObject, oRequest) {
 
-			// unbind the view to not show this object again...
-			//this.getView().unbindObject();
-			console.log("onCreateEntrySuccess");
-
-		},
-		
-		
-		_onCreateSuccess : function(oObject, oRequest) {
-
-			// unbind the view to not show this object again...
-			//this.getView().unbindObject();
+		_onCreateSuccess: function(oObject, oRequest) {
 			console.log("Success");
-
 		},
-		
+
 		_onCreateError: function(oError) {
-			
-			console.log("onCreateError");	
-		},
-		
-		_onCreateEntryError : function(oObject) {
-			
-			console.log("onCreateEntryError");
+			console.log("onCreateError");
 		},
 
-		
-		onSave:  function() {
+		onSave: function() {
 
-			var that = this,
-				oComponent = this.getOwnerComponent(),
-				oBundle = this.getResourceBundle(),
-				oModel = this.getModel();
-
-				// attach to the request completed event of the batch
-				oModel.attachEventOnce("batchRequestCompleted", function(oEvent){
-					var oParams = oEvent.getParameters();
-					if(oParams.success) {
-						that._onCreateSuccess();
-					} else {
-						that._onCrudError(); // never tested this ... central error handler maybe better here....
-					}
-				});
-		
+			var	oModel = this.getModel();
 			// submit changes to server
 			oModel.submitChanges();
-			
 		},
-		
+
 		onReset: function() {
-			
-			var oModel = this.getModel(); 
-			// var sPath = this._getPathInfo();
-			//oModel.resetChanges([sPath]);
-			
-			this.onAdd();
-			
+
+			var oModel = this.getModel();
+			var sPath = this._getPathInfo();
+			oModel.resetChanges([sPath]);
+
 			// should be false
 			console.log("After reset: Model has pending changes:", oModel.hasPendingChanges());
-			
+
 			// should contain an empty object
 			console.log("Pending changes:", oModel.getPendingChanges());
 		}

cb637ab59fd0ba4a83e77ccf4ccc10f6227b7cd7
Denise Nepraunig <denise.nepraunig@sap.com>
2016-01-12 03:27:45 +0100

    local odata examples, Web IDE git sucks

---
+++ _local-odata-model-edit-supplier-batch-false/webapp/controller/Master.controller.js
@@ -0,0 +1,78 @@
+sap.ui.define([
+	"sapui5/demo/odata/readingdata/bestpractice/controller/BaseController",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function(BaseController, MessageToast, MessageBox) {
+	"use strict";
+
+	return BaseController.extend("sapui5.demo.odata.readingdata.bestpractice.controller.Master", {
+
+		onInit: function() {
+
+			this.getRouter().getRoute("master").attachPatternMatched(this.onEdit, this);
+		},
+
+		onEdit: function() {
+
+			this._bindView("/Suppliers(1)");
+		},
+
+		_onBatchRequestCompleted: function(oEvent) {
+
+			var oParams = oEvent.getParameters();
+			if(!oParams.success) {
+				this._onEditError();
+				return;
+			}
+
+			MessageToast.show("Successfully edited entry!");
+
+			console.log("onUpdateEntrySuccess");
+		},
+
+		_bindView : function (sObjectPath) {
+			var oView = this.getView();
+			oView.bindElement(sObjectPath);
+		},
+
+		_onEditError: function(oError) {
+			
+			MessageBox.error(
+				"Error updating entry: " + oError.statusCode + " (" + oError.statusText + ")",
+				{
+					details: oError.responseText
+				}
+			);
+			console.log("onEditEntryError", oError);
+		},
+
+		_getPathInfo: function() {
+
+			var oContext = this.getView().getBindingContext();
+			return oContext.getPath();
+		},
+
+		onSave: function() {
+
+			var	oModel = this.getModel();
+			
+			oModel.attachEventOnce("requestCompleted", this._onBatchRequestCompleted.bind(this));
+			// submit changes to server
+			oModel.submitChanges();
+		},
+
+		onReset: function() {
+
+			var oModel = this.getModel();
+			var sPath = this._getPathInfo();
+			oModel.resetChanges([sPath]);
+
+			// should be false
+			console.log("After reset: Model has pending changes:", oModel.hasPendingChanges());
+
+			// should contain an empty object
+			console.log("Pending changes:", oModel.getPendingChanges());
+		}
+	});
+
+});
---
+++ _local-odata-model-write-edit-delete-supplier/webapp/controller/Master.controller.js
@@ -0,0 +1,145 @@
+sap.ui.define([
+	"sapui5/demo/odata/readingdata/bestpractice/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
+	"sap/m/MessageBox"
+], function(BaseController, JSONModel, MessageToast, MessageBox) {
+	"use strict";
+
+	return BaseController.extend("sapui5.demo.odata.readingdata.bestpractice.controller.Master", {
+
+		onInit: function() {
+
+			var oModel = new sap.ui.model.json.JSONModel({
+				createMode : true
+			});
+			this.getView().setModel(oModel, "viewModel");
+
+			this.getRouter().getRoute("master").attachPatternMatched(this.onAdd, this);
+			// this.getModel() of BaseController does not work here!
+			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+
+		},
+
+		_onMetadataLoaded: function() {
+
+			this._bMetadtaLoaded = true;
+			if (this._bRoutingReady) {
+				this._createEntry();
+			}
+		},
+
+		onAdd: function() {
+
+			this._bRoutingReady = true;
+
+			if (this._bMetadtaLoaded) {
+				this._createEntry();
+			}
+		},
+
+		_createEntry: function() {
+
+			var oModel = this.getModel();
+
+			var oView = this.getView();
+			var oContext = oModel.createEntry("Suppliers", {
+				success: this._onCreateEntrySuccess.bind(this),
+				error: this._onCreateEntryError.bind(this )
+			});
+			oView.setBindingContext(oContext);
+		},
+
+		_onCreateEntrySuccess: function(oObject, oResponse) {
+
+			MessageToast.show("Successfully created new entry!");
+
+			var sUri = oObject.__metadata.uri;
+			var aUriParts = sUri.split("/");
+			var sNewEntry = "/" + aUriParts.pop();
+
+			this.getView().unbindObject();
+			this._bindView(sNewEntry);
+
+			console.log("onCreateEntrySuccess");
+		},
+
+		_bindView : function (sObjectPath) {
+			var oView = this.getView();
+			oView.bindElement(sObjectPath);
+			this.getView().getModel("viewModel").setProperty("/createMode", false);
+		},
+
+		_onCreateEntryError: function(oError) {
+			
+			MessageBox.error(
+				"Error creating entry: " + oError.statusCode + " (" + oError.statusText + ")",
+				{
+					details: oError.responseText
+				}
+			);
+			console.log("onCreateEntryError", oError);
+		},
+
+		_getPathInfo: function() {
+
+			var oContext = this.getView().getBindingContext();
+			return oContext.getPath();
+		},
+
+		onSave: function() {
+
+			var	oModel = this.getModel();
+			// submit changes to server
+			oModel.submitChanges();
+		},
+
+		onReset: function() {
+
+			var oModel = this.getModel();
+			var sPath = this._getPathInfo();
+			oModel.resetChanges([sPath]);
+
+			// should be false
+			console.log("After reset: Model has pending changes:", oModel.hasPendingChanges());
+
+			// should contain an empty object
+			console.log("Pending changes:", oModel.getPendingChanges());
+		},
+
+		onDelete: function() {
+
+			var oModel = this.getModel();
+			var sPath = this._getPathInfo();
+
+			var oView = this.getView();
+
+			var that = this;
+
+			oModel.remove(sPath, {
+			success: function () {
+
+				MessageToast.show("Successfully deleted entry!");
+
+				oView.unbindObject();
+				that._createEntry();
+				console.log("onDeleteEntrySuccess");
+
+				that.getView().getModel("viewModel").setProperty("/createMode", true);
+			},
+
+			error: function (oError) {
+
+				MessageBox.error(
+					"Error deleting entry: " + oError.statusCode + " (" + oError.statusText + ")",
+					{
+						details: oError.responseText
+					}
+				);
+				console.log("onDeleteEntryError", oError);
+			}
+		});
+		}
+	});
+
+});

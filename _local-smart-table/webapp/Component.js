9247b84f96c927c2784efc78575b469f39d29259
Denise Nepraunig <denise.nepraunig@sap.com>
2016-05-08 00:51:02 +0200

    Smart Table rough example

---
+++ _local-smart-table/webapp/Component.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+], function(UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.smartControls.Component", {
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+			
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+	});
+
+});
---
+++ _local-smart-table/webapp/localService/mockserver.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+	"sap/ui/core/util/MockServer"
+], function (MockServer) {
+	"use strict";
+
+	return {
+
+		init: function () {
+
+			// create
+			var oMockServer = new MockServer({
+				rootUri: "/here/goes/your/serviceUrl/"
+			});
+
+			// configure
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: 1000
+			});
+
+			// simulate
+			var sPath = jQuery.sap.getModulePath("sap.ui.demo.smartControls.localService");
+			oMockServer.simulate(sPath + "/metadata.xml", sPath);
+
+			// start
+			oMockServer.start();
+		}
+	};
+
+});

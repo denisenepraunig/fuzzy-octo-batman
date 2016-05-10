f1ba5e549bdcb11efede855cbc18e779d6994dba
Denise Nepraunig <denise.nepraunig@sap.com>
2016-05-10 22:45:09 +0200

    smart controls variant managment, table without personalisation

---
+++ _local-smart-table-with-variant-management/webapp/localService/mockserver.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+	"sap/ui/core/util/MockServer"
+], function (MockServer) {
+	"use strict";
+
+	return {
+
+		init: function () {
+
+			var sModulePath = jQuery.sap.getModulePath("sap.ui.demo.smartControls.localService");
+
+			// enable fake lrep for variant management
+			jQuery.sap.require("sap.ui.fl.FakeLrepConnector");
+			var sFakeLrepSettingsPath = sModulePath + "/component-test-changes.json";
+			sap.ui.fl.FakeLrepConnector.enableFakeConnector(sFakeLrepSettingsPath);
+
+			// create
+			var oMockServer = new MockServer({
+				rootUri: "/here/goes/your/serviceUrl/"
+			});
+
+			// configure
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: 1000
+			});
+
+			// simulate
+			oMockServer.simulate(sModulePath + "/metadata.xml", sModulePath);
+
+			// start
+			oMockServer.start();
+		}
+	};
+
+});
---
+++ _local-smart-table-without-personalisation/webapp/controller/Master.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/demo/smartControls/controller/BaseController"
+], function(BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.demo.smartControls.controller.Master", {
+		
+	});
+
+});

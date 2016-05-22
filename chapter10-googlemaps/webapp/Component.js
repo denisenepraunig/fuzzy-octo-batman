14c273ee6bf154da1e79be18e86ee28bf34514fd
Christiane Kurz <christiane.kurz@sap.com>
2016-05-22 22:40:59 +0000

    chapter 10 googlemaps control

--- chapter10-googlemaps/webapp/Component.js
+++ chapter10-googlemaps/webapp/Component.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(UIComponent) {
 	"use strict";
 
-	return UIComponent.extend("sapui5.demo.odata.readingdata.bestpractice.Component", {
+	return UIComponent.extend("sapui5.demo.advanced.fragments.Component", {
 
 		metadata: {
 			manifest: "json"
@@ -17,15 +17,9 @@ sap.ui.define([
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-			jQuery.sap.registerResourcePath("userDetails", "../../userdetails");
+			
 			// create the views based on the url/hash
 			this.getRouter().initialize();
-			// create the views based on the url/hash
-			this.getRouter().initialize();
-			this.userComponent = sap.ui.getCore().createComponent({
-                 name: "sapui5.demo.userdetails",
-                 id: "Comp1"
-        });
 		}
 	});
 
